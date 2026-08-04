bfb34d3298f656bea8a89f833cd48cf1017b09ed
Rasim Ablyalimov <rasim.a@wecandevelopit.com>
2026-08-04 10:21:36 +0300

    add server requests error handler

--- frontend/src/entities/service/api/getServices.ts
+++ frontend/src/entities/service/api/getServices.ts
@@ -2,11 +2,15 @@ import { client } from "@/shared/api"
 import { QueryKeys } from "@/shared/config"
 
 export async function getServices() {
-  const { data, error } = await client.GET("/services", {
-    baseUrl: process.env.INTERNAL_API_URL || "http://backend:8000",
-    cache: "force-cache",
-    next: { tags: [QueryKeys.SERVICES] },
-  })
-  if (error) throw error
-  return data.data ?? []
+  try {
+    const { data, error } = await client.GET("/services", {
+      baseUrl: process.env.INTERNAL_API_URL || "http://backend:8000",
+      cache: "force-cache",
+      next: { tags: [QueryKeys.SERVICES] },
+    })
+    if (error) throw error
+    return data.data ?? []
+  } catch {
+    return []
+  }
 }
--- frontend/src/entities/test/api/getTest.ts
+++ frontend/src/entities/test/api/getTest.ts
@@ -1,13 +1,17 @@
 import { client } from "@/shared/api"
 
 export async function getTest() {
-  const { data, error, response } = await client.GET("/tests", {
-    baseUrl: process.env.INTERNAL_API_URL || "http://backend:8000",
-    cache: "force-cache",
-  })
-  if (error) {
-    if (response.status === 404) return null
-    throw error
+  try {
+    const { data, error, response } = await client.GET("/tests", {
+      baseUrl: process.env.INTERNAL_API_URL || "http://backend:8000",
+      cache: "force-cache",
+    })
+    if (error) {
+      if (response.status === 404) return null
+      throw error
+    }
+    return data?.data ?? null
+  } catch {
+    return null
   }
-  return data?.data ?? null
 }
